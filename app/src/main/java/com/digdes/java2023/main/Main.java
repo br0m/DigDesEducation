52d85cc6e096ff6cd148a221a6b6cc105e1aeaa8
Sergey Korostelev <br0m2@yandex.ru>
2023-05-15 14:42:09 +0700

    add sample code

--- app/src/main/java/com/digdes/java2023/main/Main.java
+++ app/src/main/java/com/digdes/java2023/main/Main.java
@@ -1,7 +1,36 @@
 package com.digdes.java2023.main;
 
+import com.digdes.java2023.dto.enums.MemberStatus;
+import com.digdes.java2023.model.Member;
+import com.digdes.java2023.repositories.MemberRepository;
+import com.digdes.java2023.repositories.impl.MemberRepositoryImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+
+        MemberRepository memberRepository = new MemberRepositoryImpl();
+
+        Member member = new Member();
+        member.setFirstName("Иван");
+        member.setLastName("Иванов");
+        member.setPatronymic("Иванович");
+        member.setAccount("ivanov");
+        member.setJobTitle("Developer");
+        member.setEmail("ivanov@mail.ru");
+        member.setStatus(MemberStatus.ACTIVE);
+        member=memberRepository.createMember(member);
+
+        Member member2 = new Member();
+        member2.setId(0);
+        member2.setJobTitle("Tester");
+        member2=memberRepository.updateMember(member2);
+
+        List<Member> list = memberRepository.findMember("Иван");
+
+        Member member3 = memberRepository.getById(1);
+        Member member4 = memberRepository.deleteById(0);
+
     }
 }
