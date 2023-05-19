865bd31710ebaaf2be00096abea58be9fd72fd68
Sergey Korostelev <br0m2@yandex.ru>
2023-05-19 18:19:42 +0700

    add sample code

--- app/src/main/java/com/digdes/java2023/main/Main.java
+++ app/src/main/java/com/digdes/java2023/main/Main.java
@@ -1,17 +1,21 @@
 package com.digdes.java2023.main;
 
+import com.digdes.java2023.dto.enums.MemberRole;
 import com.digdes.java2023.dto.enums.MemberStatus;
 import com.digdes.java2023.model.Member;
+import com.digdes.java2023.model.Project;
 import com.digdes.java2023.repositories.MemberRepository;
-import com.digdes.java2023.repositories.impl.MemberRepositoryImpl;
+import com.digdes.java2023.repositories.TeamRepository;
+import com.digdes.java2023.repositories.impl.MemberRepositoryJdbcImpl;
+import com.digdes.java2023.repositories.impl.TeamRepositoryJdbcImpl;
 
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
 
-        MemberRepository memberRepository = new MemberRepositoryImpl();
-
+        MemberRepository memberRepository = new MemberRepositoryJdbcImpl("jdbc:postgresql://localhost:5432/digdes_project", "postgres", "admin");
         Member member = Member.builder().firstName("Иван").lastName("Иванов").patronymic("Иванович").account("ivanov").jobTitle("Developer").email("ivanov@mail.ru").status(MemberStatus.ACTIVE).build();
         member=memberRepository.createMember(member);
 
@@ -22,6 +26,11 @@ public class Main {
 
         Member member3 = memberRepository.getById(member2.getId());
         Member member4 = memberRepository.deleteById(member2.getId());
+        list = memberRepository.getAll();
 
+        TeamRepository teamRepository = new TeamRepositoryJdbcImpl("jdbc:postgresql://localhost:5432/digdes_project", "postgres", "admin");
+        Project project = new Project();
+        project.setId(2);
+        Map<Member, MemberRole> map = teamRepository.getProjectMembers(project);
     }
 }
