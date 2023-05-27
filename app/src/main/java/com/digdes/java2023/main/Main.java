07c02b92027bf090ade25900ffde00dd6bc0ea48
Sergey Korostelev <br0m2@yandex.ru>
2023-05-27 23:33:20 +0700

    add spring boot, fix dependencies

--- app/src/main/java/com/digdes/java2023/main/Main.java
+++ app/src/main/java/com/digdes/java2023/main/Main.java
@@ -1,44 +1,16 @@
 package com.digdes.java2023.main;
 
-import com.digdes.java2023.dto.enums.MemberRole;
-import com.digdes.java2023.dto.enums.MemberStatus;
-import com.digdes.java2023.model.Member;
-import com.digdes.java2023.model.Project;
-import com.digdes.java2023.repositories.MemberRepository;
-import com.digdes.java2023.repositories.TeamRepository;
-import com.digdes.java2023.repositories.impl.MemberRepositoryJdbcImpl;
-import com.digdes.java2023.repositories.impl.TeamRepositoryJdbcImpl;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.List;
-import java.util.Map;
-
+@SpringBootApplication (scanBasePackages = "com.digdes.java2023")
+@EntityScan(basePackages = "com.digdes.java2023.model")
+@EnableJpaRepositories(basePackages = "com.digdes.java2023.repositories")
 public class Main {
+
     public static void main(String[] args) {
-
-        try(Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/digdes_project", "postgres", "admin")){
-
-            MemberRepository memberRepository = new MemberRepositoryJdbcImpl(connection);
-            Member member = Member.builder().firstName("Иван").lastName("Иванов").patronymic("Иванович").account("ivanov").jobTitle("Developer").email("ivanov@mail.ru").status(MemberStatus.ACTIVE).build();
-            member=memberRepository.createMember(member);
-
-            Member member2 = Member.builder().id(member.getId()).firstName(member.getFirstName()).lastName(member.getLastName()).patronymic(member.getPatronymic()).jobTitle("Tester").status(member.getStatus()).build();
-            member2=memberRepository.updateMember(member2);
-
-            List<Member> list = memberRepository.findMember(member.getFirstName());
-
-            Member member3 = memberRepository.getById(member2.getId());
-            Member member4 = memberRepository.deleteById(member2.getId());
-            list = memberRepository.getAll();
-
-            TeamRepository teamRepository = new TeamRepositoryJdbcImpl(connection);
-            Project project = new Project();
-            project.setId(2);
-            Map<Member, MemberRole> map = teamRepository.getProjectMembers(project);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        SpringApplication.run(Main.class, args);
     }
 }
