eaa9c6a3fca3985fbf4591e6de3ac7673b0e3568
Sergey Korostelev <br0m2@yandex.ru>
2023-06-13 11:55:16 +0700

    add @EqualsAndHashCode to dto

--- dto/src/main/java/com/digdes/java2023/dto/project/ProjectDto.java
+++ dto/src/main/java/com/digdes/java2023/dto/project/ProjectDto.java
@@ -3,11 +3,13 @@ package com.digdes.java2023.dto.project;
 import com.digdes.java2023.dto.enums.ProjectStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode
 @Schema(description = "DTO проекта")
 public class ProjectDto {
 
