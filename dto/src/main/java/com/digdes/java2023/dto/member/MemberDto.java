6da678dd6fe13d2d8620f562262e0f98a23b35ab
Sergey Korostelev <br0m2@yandex.ru>
2023-06-13 11:43:03 +0700

    remove builder from dto

--- dto/src/main/java/com/digdes/java2023/dto/member/MemberDto.java
+++ dto/src/main/java/com/digdes/java2023/dto/member/MemberDto.java
@@ -8,17 +8,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.experimental.SuperBuilder;
-import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
-@SuperBuilder
-@NoArgsConstructor
 @EqualsAndHashCode
-@Component
 @Schema(description = "DTO сотрудника")
 public class MemberDto {
 
